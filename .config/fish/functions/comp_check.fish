7837a4f9d3e3b5337ba91d2166c6288fa1a562dc
Dev <flashytesting@gmail.com>
2022-04-18 11:40:51 +0530

    Stop from making another dir inside the already available dir

--- .config/fish/functions/comp_check.fish
+++ .config/fish/functions/comp_check.fish
@@ -3,14 +3,26 @@ function comp_check
 
     switch $EXT
         case c
-            mkdir c_dir && mv $argv c_dir && cd c_dir
-            gcc $argv && ./a.out
+            if test -d ../c_dir
+                gcc $argv && ./a.out
+            else
+                mkdir c_dir && mv $argv c_dir && cd c_dir
+                gcc $argv && ./a.out
+            end
         case cpp
-            mkdir cpp_dir && mv $argv cpp_dir && cd cpp_dir
-            g++ $argv && ./a.out
+            if test -d ../cpp_dir
+                g++ $argv && ./a.out
+            else
+                mkdir cpp_dir && mv $argv cpp_dir && cd cpp_dir
+                g++ $argv && ./a.out
+            end
         case asm
-            mkdir asm_dir && mv $argv asm_dir && cd asm_dir
-            nasm -f elf32 $argv && ld -m elf_i386 (echo $argv | cut -d. -f1).o && ./a.out
+            if test -d ../asm_dir
+                nasm -f elf32 $argv && ld -m elf_i386 (echo $argv | cut -d. -f1).o && ./a.out
+            else
+                mkdir asm_dir && mv $argv asm_dir && cd asm_dir
+                nasm -f elf32 $argv && ld -m elf_i386 (echo $argv | cut -d. -f1).o && ./a.out
+            end
         case '*'
             return 1
     end
