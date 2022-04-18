ae1a0d3f6dc00012faa96c042e429ce7d38df870
Dev <flashytesting@gmail.com>
2022-04-18 10:51:36 +0530

    Add separate EXT dependent dir for file management

--- .config/fish/functions/comp_check.fish
+++ .config/fish/functions/comp_check.fish
@@ -3,10 +3,13 @@ function comp_check
 
     switch $EXT
         case c
+            mkdir c_dir && mv $argv c_dir && cd c_dir
             gcc $argv && ./a.out
         case cpp
+            mkdir cpp_dir && mv $argv cpp_dir && cd cpp_dir
             g++ $argv && ./a.out
         case asm
+            mkdir asm_dir && mv $argv asm_dir && cd asm_dir
             nasm -f elf32 $argv && ld -m elf_i386 (echo $argv | cut -d. -f1).o && ./a.out
         case '*'
             return 1
