e71d2fe0b7658e143a4279327f607d7ab6a0c0c8
Dev <flashytesting@gmail.com>
2022-05-10 00:58:07 +0530

    Add checker for rust files

--- .config/fish/functions/comp_check.fish
+++ .config/fish/functions/comp_check.fish
@@ -1,5 +1,6 @@
 function comp_check
     set EXT (echo $argv | cut -d. -f2)
+    set NAME (echo $argv | cut -d. -f1)
 
     switch $EXT
         case c
@@ -22,6 +23,13 @@ function comp_check
             else
                 mkdir asm_dir && mv $argv asm_dir && cd asm_dir
                 nasm -f elf32 $argv && ld -m elf_i386 (echo $argv | cut -d. -f1).o && ./a.out
+            end
+        case rs
+            if test -d ../rust_dir
+                rustc $argv && ./$NAME
+            else
+                mkdir rust_dir && mv $argv rust_dir && cd rust_dir
+                rustc $argv && ./$NAME
             end
         case '*'
             return 1
